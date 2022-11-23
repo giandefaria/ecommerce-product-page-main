c3da45105e305f73f960dde69c0fcd6cb4d2a00f
Gian <gian.defaria@hotmail.com>
2022-11-22 21:33:54 -0300

    Adicionei a mensagem de carrinho vazio na caixa do carrinho. Adicionei função que adiciona  o produto e a quantidade no carrinho após clicar no botao add to cart, adicionei função no ícone lixeira para limpar os itens do carrinho, adicionei função para abrir e fechar o carrinho ao clicar no icone do carrinho

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ const allThumbnail = document.querySelectorAll(".all--images");
 const quantidity = document.getElementById('quantidity');
 const iconMinus = document.getElementsByClassName('icon--minus');
 const iconPlus = document.getElementsByClassName('icon--plus');
-let selectedImage = "images/image-product-1.jpg" 
+const cartIcon = document.getElementsByClassName('cart--notification');
 const cartBadge = document.getElementsByClassName('badge');
+const cartBox = document.getElementById('cart--box');
 const addCartButton = document.getElementsByClassName('add--to--cart');
 const cartValueandQuantidity = document.getElementsByClassName('value--and--multipler');
+const cartTotal = document.getElementsByClassName("cart--total");
 const clearCart = document.getElementsByClassName('delete');
+const cartEmpty = document.getElementById("cart--empty");
+const cartFull = document.getElementById("container");
 
+let selectedImage = "images/image-product-1.jpg" 
 let value = 0;
 let cartQuantidity = 0;
 
@@ -54,27 +59,39 @@ iconPlus[0].addEventListener('click', ()=> {
     }
 })
 
+//open cart box on click
+cartIcon[0].addEventListener('click', () => {
+    cartBox.classList.toggle("cart--box--active");
+})
+
 //function add price, quantidity and total value in cart box
 function totalValue () {
-    cartValueandQuantidity[0].innerHTML = '$125.00 x ' + cartQuantidity + " " + "$" + (125.00 * cartQuantidity).toFixed(2);
-
+    cartValueandQuantidity[0].innerHTML = '$125.00 x ' + cartQuantidity + " ";
+    cartTotal[0].innerHTML = " $" + (125.00 * cartQuantidity).toFixed(2);
 }
 
+//add quantidity to cart on click in add to cart button
 addCartButton[0].addEventListener('click', () => {
 
         cartQuantidity = cartQuantidity + value;        
 
         cartBadge[0].innerHTML = cartQuantidity;
         totalValue();
+        cartFull.classList.replace('container', 'container--active');
+        cartEmpty.classList.replace('cart--empty--active','cart--empty');
 
 })
 
+//clear cart value on click
 clearCart[0].addEventListener('click', () => {
     value = 0;
     cartQuantidity = 0;
     quantidity.innerHTML = value;
     cartBadge[0].innerHTML = ""
     totalValue();
+    cartFull.classList.replace('container--active', 'container');
+    cartEmpty.classList.replace('cart--empty','cart--empty--active');
 
 })
+
 
